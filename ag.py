fb5d4495ba6e4dd36b8f796fd45603aec6e3fa02
James Jungwirth <james@illuminatedcomputing.com>
2021-02-05 20:29:10 -0800

    version 1

---
+++ ag.py
@@ -0,0 +1,14 @@
+print("Hello welcome to the Agricola score keeper")
+while True:
+
+
+  print("push 1 to look at old games push 2 to  add a new game")
+
+
+  choice = raw_input('')
+  if choice=="1":
+    print("no games yet")
+  elif choice=="2":
+    score=raw_input("what is your score")
+  else:
+    print("What human are you stupid?!?")
