b979f0595e7b2f972158075d662af57f8765d7d2
James Jungwirth <james@illuminatedcomputing.com>
2021-02-05 21:18:25 -0800

    remember your scores

--- ag.py
+++ ag.py
@@ -1,14 +1,17 @@
+scores =[]
 print("Hello welcome to the Agricola score keeper")
+
 while True:
-
-
   print("push 1 to look at old games push 2 to  add a new game")
-
-
-  choice = raw_input('')
+  choice =input('')
   if choice=="1":
-    print("no games yet")
+    if len(scores)==0:
+      print("no games yet")
+    else:
+      print("%r" % scores)
   elif choice=="2":
-    score=raw_input("what is your score")
+    score=input("what is your score")
+    scores.append(score)
   else:
     print("What human are you stupid?!?")
+
