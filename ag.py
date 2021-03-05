10391d37745057cf995c23a3f78032c37a64310e
James Jungwirth <james@illuminatedcomputing.com>
2021-03-04 18:08:02 -0800

    We made it ask for the players and their scores.

--- ag.py
+++ ag.py
@@ -1,3 +1,11 @@
+from dataclasses import dataclass, field
+from typing import List
+
+@dataclass
+class Game:
+  names: List[str] = field(default_factory=list)
+  scores: List[int] = field(default_factory=list)
+
 def load_scores():
   ret=[]
   f=open("Agricola_scores", "r")
@@ -14,7 +22,8 @@ def save_scores(new_scores):
     f.write("%d\n" % score)
   f.close()
 
-scores =load_scores()
+#scores =load_scores()
+scores=[]
 print("Hello welcome to the Agricola score keeper")
 
 while True:
@@ -24,12 +33,28 @@ while True:
     if len(scores)==0:
       print("no games yet")
     else:
-      for score in scores:
-        print(score)
+      # for score in scores:
+      #  print(score)
+      print("%r"%scores)
   elif choice=="2":
-    score=input("what is your score ")
-    score=int(score)
-    scores.append(score)
-    save_scores(scores)
+    g=Game()
+    print("Who are the players?")
+    print("push enter to stop")
+    name=input("")
+    while name !="":
+         g.names.append(name)
+         name=input("")
+    # score=input("what is your score ")
+    #  score=int(score)
+    #  scores.append(score)
+    # save_scores(scores)
+    print("What are your scores")
+    print("push enter to stop")
+    number=input("")
+    while number !="":
+      g.scores.append(int(number))
+      number=input("")
+    scores.append(g) 
   else:
+
     print("What human are you stupid?!?")
